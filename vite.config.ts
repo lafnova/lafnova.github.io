74ed2719b3a0d0aca406a265cbeb8077ea77c49b
rafi1545713-tech <rafi1545713@gmail.com>
2026-07-14 02:37:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { tanstackStartVite } from '@tanstack/start-vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tanstackStartVite(),
+  ],
 })
